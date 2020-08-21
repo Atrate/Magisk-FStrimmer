55cd135fbfe1cbe36291072db7f70500552c7099
Atrate <Atrate@protonmail.com>
2020-08-22 00:10:26 +0200

    Add copyright (copyleft) notice
    Signed-off-by: Atrate <Atrate@protonmail.com>

--- service.sh
+++ service.sh
@@ -1,4 +1,16 @@
 #!/system/bin/sh
+
+# Copyright (C) 2020 Atrate <atrate@protonmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 LOGFILE='/cache/fstrimmer_lastrun.log'
 
 # Sleep for 2 minutes due to a lot of I/O happening shortly after boot
